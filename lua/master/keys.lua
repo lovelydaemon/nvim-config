52ba9f756b53b1de6da3dde38c47fb8efb076e73
lovelydaemon <slamy777@gmail.com>
2023-11-02 23:42:29 +0300

    feat: add replace all hotkey

--- lua/master/keys.lua
+++ lua/master/keys.lua
@@ -53,3 +53,6 @@ vim.keymap.set('v', '<leader>{', 'vbi{<Esc>ea}<Esc>')
 --Git
 vim.keymap.set('n', ']c', ':Git next_hunk<CR>')
 vim.keymap.set('n', '[c', ':Git prev_hunk<CR>')
+
+-- replace all
+vim.keymap.set('n', '<leader>ra', '* :%s/')
