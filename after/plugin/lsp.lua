0774ad50c0f02c85024fa6e28fd27c4994637611
lovelydaemon <slamy777@gmail.com>
2023-11-06 03:45:02 +0300

    feat: update snippets

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -47,14 +47,14 @@ local cmp_maps = {
   ['<Tab>'] = cmp.mapping(function(fallback)
     if cmp.visible() then
       cmp.confirm({ select = true })
-    elseif luasnip.jumpable(1) then
+    elseif luasnip.locally_jumpable(1) then
       luasnip.jump(1)
     else
       fallback()
     end
   end, { "i", "s" }),
   ['<S-Tab>'] = cmp.mapping(function(fallback)
-    if luasnip.jumpable(-1) then
+    if luasnip.locally_jumpable(-1) then
       luasnip.jump(-1)
     else
       fallback()
@@ -130,5 +130,6 @@ lsp_zero.format_on_save({
     ['cssls'] = { 'css', 'scss' },
     ['tsserver'] = { 'javascript', 'typescript' },
     ['ruby_ls'] = { 'ruby' },
+    ['jsonls'] = { 'json' }
   }
 })
