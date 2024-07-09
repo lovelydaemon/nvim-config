63928ba3964d779f5c89b1f93c9fcd40f537cbad
lovelydaemon <slamy777@gmail.com>
2024-07-09 11:22:15 +0300

    feat(main): show file and parent directory

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,22 +1,22 @@
-local lualine = require'lualine'
+local lualine = require 'lualine'
 
 local diff = {
   'diff',
   colored = false,
-  symbols = {added = ' ', modified = ' ', removed = ' ' },
+  symbols = { added = ' ', modified = ' ', removed = ' ' },
 }
 
 local filename = {
   'filename',
-  symbols = {modified = ' ', readonly = ' ', unnamed = '[No name]'},
+  symbols = { modified = ' ', readonly = ' ', unnamed = '[No name]' },
   path = 0
 }
 
 local diagnostics = {
   'diagnostics',
-  sources = {'nvim_diagnostic'},
-  sections = {'error', 'warn'},
-  symbols = {error = ' ', warn = ' '},
+  sources = { 'nvim_diagnostic' },
+  sections = { 'error', 'warn' },
+  symbols = { error = ' ', warn = ' ' },
   colored = true,
 }
 
@@ -24,27 +24,27 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = {left = '', right = ''},
-    section_separators = {left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {},
     always_divide_middle = true,
     globalstatus = false,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', diff},
-    lualine_c = {filename},
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', diff },
+    lualine_c = { { 'filename', path = 4 } },
 
-    lualine_x = {diagnostics},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_x = { diagnostics },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {filename},
+    lualine_c = { filename },
 
-    lualine_x = {'location'},
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
