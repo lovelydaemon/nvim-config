fc1a72120098d69e6671deb7d958b5b3f2c06311
lovelydaemon <slamy777@gmail.com>
2024-09-03 20:57:55 +0300

    misc(main): update telescope version

--- lua/master/packer.lua
+++ lua/master/packer.lua
@@ -5,7 +5,7 @@ return require 'packer'.startup(function(use)
   use 'wbthomason/packer.nvim'
 
   -- Telescope
-  use { 'nvim-telescope/telescope.nvim', tag = '0.1.2', requires = { { 'nvim-lua/plenary.nvim' } }
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { { 'nvim-lua/plenary.nvim' } }
   }
 
   -- Colorscheme(s)
