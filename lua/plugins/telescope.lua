748acfd936f59fbc982156a9ab8e99c66a9058eb
lovelydaemon <slamy777@gmail.com>
2025-02-25 18:35:19 +0300

    misc: grep string instantly opens telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -22,8 +22,6 @@ return {
 
     vim.keymap.set('n', '<leader>pf', builtin.git_status, {})
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input('Grep > ') });
-    end)
+    vim.keymap.set('n', '<leader>ps', builtin.grep_string, {})
   end,
 }
