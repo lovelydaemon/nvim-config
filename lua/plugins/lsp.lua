6da6ca7dc25466aa5ed1118bdadfe2856e413415
lovelydaemon <slamy777@gmail.com>
2025-04-14 20:38:57 +0300

    feat: docker compose lsp support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -180,6 +180,13 @@ return {
               }
             })
           end,
+          docker_compose_language_service = function()
+            lspconfig.docker_compose_language_service.setup({
+              filetypes = {
+                "yaml"
+              }
+            })
+          end,
           lua_ls = function()
             lspconfig.lua_ls.setup({
               settings = {
