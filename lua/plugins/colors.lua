a61a171fbb190cb99bf0cdaf7226ef644644c791
lovelydaemon <slamy777@gmail.com>
2025-08-11 11:16:06 +0300

    feat: restore colors

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -12,8 +12,8 @@ return {
       bg_dim = { '#201E28', '232' },
       bg0 = { '#201E28', '235' },
       bg2 = { '#2C2936', '236' },
-      bg_blue = { '#7E9CD8', '110' },
-      bg_green = { '#98BB6C', '107' },
+      --bg_blue = { '#7E9CD8', '17' },
+      --bg_green = { '#394634', '22' },
       fg = { '#DCD7BA', '250' },
       red = { '#E46876', '203' },
       blue = { '#79AAD0', '110' },
@@ -21,6 +21,9 @@ return {
       yellow = { '#E2BD5C', '179' },
       orange = { '#D98C5F', '215' },
       purple = { '#9682B5', '176' },
+      filled_green = { '#96B369', '107' },
+      filled_blue = { '#7E9CD8', '110' },
+      filled_red = { '#E46876', '203' },
     }
 
     vim.cmd([[colorscheme sonokai]])
