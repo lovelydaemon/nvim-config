353e4deb76a5de71b684f9788ec77e013e107f06
lovelydaemon <slamy777@gmail.com>
2026-08-18 13:12:55 +0300

    feat: restore branch

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,44 +1,44 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons'
-  },
-  event = "VeryLazy",
-  opts = function()
-    local diagnostics = {
-      'diagnostics',
-      sources = { 'nvim_diagnostic' },
-      sections = { 'error', 'warn' },
-      symbols = { error = ' ', warn = ' ' },
-      colored = true,
-    }
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	event = "VeryLazy",
+	opts = function()
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " " },
+			colored = true,
+		}
 
-    return {
-      options = {
-        icons_enabled = true,
-        theme = 'auto',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        always_divide_middle = true,
-        globalstatus = true,
-      },
-      sections = {
-        lualine_a = { 'mode' },
-        lualine_b = {},
-        lualine_c = { { 'filename', path = 4 } },
+		return {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				always_divide_middle = true,
+				globalstatus = false,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = { { "filename", path = 4 } },
 
-        lualine_x = { diagnostics },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { { 'filename', path = 4 } },
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {}
-      },
-    }
-  end
+				lualine_x = { diagnostics },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { { "filename", path = 4 } },
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		}
+	end,
 }
